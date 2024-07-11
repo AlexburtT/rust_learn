b47c9ea121bd6938cdc9a98e3617675f08331c45
AlexburtT <alexburt12634@gmail.com>
2024-07-11 18:10:04 +0900

    update if-else

--- if-else/src/main.rs
+++ if-else/src/main.rs
@@ -10,6 +10,52 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }
+
+    loop_if();
+
+    while_if();
+
+    for_if();
 }
 
 // не конвертирует нелогический тип в логический, нужно указывать явно условие. После первого совпадения, дальше не проверяет. - https://doc.rust-lang.ru/book/ch03-05-control-flow.html
+
+// Повторное выполнение кода с помощью циклов
+// Повторение выполнения кода с помощью loop
+fn loop_if() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 100 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+// Циклы с условием while
+fn while_if() {
+    let mut number = 10;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+
+// Цикл по элементам коллекции с помощью for
+
+fn for_if() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+}
